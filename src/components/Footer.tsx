ef63bd26992a0735076bc33981039e10cd5efbd3
AJ Eubanks <aeubanks322@gmail.com>
2024-12-03 23:54:42 -0500

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="hidden lg:fixed lg:bottom-0 lg:left-0 lg:block bg-transparent text-zinc-600 dark:text-zinc-400 text-xs p-2">
+      Website Created by Andrew Eubanks
+    </footer>
+  );
+};
+
+export default Footer;
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Header from './components/Header';
+import Footer from "./components/Footer";
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -12,18 +13,27 @@ const Main: React.FC = () => {
   return (
     <ThemeProvider>
       <Router basename={import.meta.env.BASE_URL}>
-        <div className="min-h-screen bg-white dark:bg-zinc-950 text-zinc-900 dark:text-zinc-100">
+        <div className="min-h-screen bg-white dark:bg-zinc-950 text-zinc-900 dark:text-zinc-100 flex flex-col">
+          {/* Header */}
           <Header />
-          <Routes>
-            <Route path={`/`} element={<Home />} />
-            <Route path={`/about`} element={<About />} />
-            <Route path={`/projects`} element={<Projects />} />
-          </Routes>
+
+          {/* Main Content */}
+          <main className="flex-grow">
+            <Routes>
+              <Route path={`/`} element={<Home />} />
+              <Route path={`/about`} element={<About />} />
+              <Route path={`/projects`} element={<Projects />} />
+            </Routes>
+          </main>
+
+          {/* Footer */}
+          <Footer />
         </div>
       </Router>
     </ThemeProvider>
   );
 };
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
